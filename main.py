14c6c24abacad6f1f4d51a9e6039a2612183a459
nikola1414 <nobradovic069@gmail.com>
2026-07-13 19:58:42 +0200

    weather getter

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 import requests
+import os
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
                               QLineEdit, QPushButton, QVBoxLayout)
 from PyQt5.QtCore import Qt
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class WeatherApp(QWidget):
     def __init__(self):
@@ -10,9 +14,9 @@ class WeatherApp(QWidget):
         self.city_label = QLabel("Unesi ime grada: ", self)
         self.city_input = QLineEdit(self)
         self.get_weather_btn = QPushButton("Enter",self)
-        self.temp_label = QLabel("29°C",self)
-        self.emoji_label = QLabel("☀️",self)
-        self.desc_label = QLabel("Suncano",self)
+        self.temp_label = QLabel(self)
+        self.emoji_label = QLabel(self)
+        self.desc_label = QLabel(self)
         self.initUI()
 
     def initUI(self):
@@ -67,7 +71,60 @@ class WeatherApp(QWidget):
             }
         """)
 
+        self.get_weather_btn.clicked.connect(self.get_weather)
 
+    def get_weather(self):
+        API_KEY = os.environ.get("WEATHER_API_KEY")
+        city = self.city_input.text()
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            
+            if data["cod"] == 200:
+                self.display_weather(data)
+                    
+        except requests.exceptions.HTTPError as http_error:
+            match response.status_code:
+                case 400:
+                    self.display_error("Bad request:\nProveriti unos")
+                case 401:
+                    self.display_error("Unauthorized:\nAPI kljuc nije validan")
+                case 403:
+                    self.display_error("Forbidden:\n Zahtev odbijen")
+                case 404:
+                    self.display_error("Not found:\nGrad nije pronadjen")
+                case 500:
+                    self.display_error("Internal Server Error:\nProbajte kasnije")
+                case 502:
+                    self.display_error("Bad Gateway:\nLos odgovor servera")
+                case 503:
+                    self.display_error("Service Unavailable:\nServer je pao")
+                case 504:
+                    self.display_error("Gateway timeout:\nNema odgovora od servera")
+                case _:
+                    self.display_error(f"HTTP error occured:\n{http_error}")
+
+        except requests.exceptions.ConnectionError:
+            self.display_error("Connection Error:\nProverite internet konekciju")
+
+        except requests.exceptions.Timeout:
+            self.display_error("Timeout Error:\nZahtev istekao")
+
+        except requests.exceptions.TooManyRedirects:
+            self.display_error("Too many Redirects:\nProverite URL")
+
+        except requests.exceptions.RequestException as req_error:
+            self.display_error(f"Request Error:\n{req_error}")
+
+    
+    def display_error(self, message):
+        pass
+
+    def display_weather(self, data):
+        print(data)
 
 
 if __name__ == "__main__":
